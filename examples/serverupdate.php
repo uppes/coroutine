5eae97ae3f2bb2cc2fa1dccb3687125695ac50fe
techno-express <technoexpressnet@gmail.com>
2019-02-15 18:51:46 -0500

    bug fix

--- examples/serverupdate.php
+++ examples/serverupdate.php
@@ -1,21 +1,12 @@
 <?php
 include 'vendor/autoload.php';
 
-//use Async\Coroutine\CoSocket;
 use Async\Coroutine\Coroutine;
 
 function server($port) {
     echo "Starting server at port $port...\n";
 
-   // $socket = @\stream_socket_server("tcp://localhost:$port", $errNo, $errStr);
-
-    //if (!$socket)
-    //    throw new \Exception($errStr, $errNo);
-
-   // \stream_set_blocking($socket, 0);
-
     $socket = \createSocket($port);
-    //$socket = new CoSocket($socket);
     while (true) {
         yield from \async('handleClient', yield \acceptSocket($socket) );
     }
