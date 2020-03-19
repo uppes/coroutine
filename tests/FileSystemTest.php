5b87fb5a6fa8988118a72f48d25271c820f8cb8c
techno-express <technoexpressnet@gmail.com>
2020-03-18 21:46:23 -0400

    Update FileSystemTest.php

--- tests/FileSystemTest.php
+++ tests/FileSystemTest.php
@@ -196,12 +196,12 @@ class FileSystemTest extends TestCase
         $data = yield \file_contents($fd);
         $this->assertEquals($text, $data);
 
-        $this->assertGreaterThanOrEqual(10, $this->counterResult);
+        $this->assertGreaterThanOrEqual(9, $this->counterResult);
 
         $moreData = yield \file_contents($fd);
         $this->assertEquals('', $moreData);
 
-        $this->assertGreaterThanOrEqual(11, $this->counterResult);
+        $this->assertGreaterThanOrEqual(10, $this->counterResult);
 
         $bool = yield \file_close($fd);
         $this->assertTrue($bool);
